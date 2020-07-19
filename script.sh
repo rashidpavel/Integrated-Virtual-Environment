f8406e303b00641897621dec0adc180a37164d89
rashidpavel <55319375+rashidpavel@users.noreply.github.com>
2020-07-19 23:03:31 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,10 +2,9 @@ adduser sysadmin --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-
 echo "sysadmin:proxmox" |  chpasswd &&
 
 apt update && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common &&
-wget https://github.com/rashidpavel/Integrated-Virtual-Environment/archive/master.zip &&
-unzip master.zip && cd Integrated-Virtual-Environment-master/ &&
 rm -rf /etc/apt/sources.list.d/pve-enterprise.list &&
 rm -rf /etc/apt/sources.list && cp -r sources.list /etc/apt/sources.list &&
+
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg &&
 
 apt update && apt -y dist-upgrade && 
