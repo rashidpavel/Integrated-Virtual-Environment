75740957debc66412fa95cb6d5164b564f1a0286
rashidpavel <55319375+rashidpavel@users.noreply.github.com>
2020-08-17 14:25:57 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,28 @@
 adduser sysadmin --gecos "Sysadmin MRPlab,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "sysadmin:proxmox" |  chpasswd &&
 
-apt update && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common curl &&
-rm -rf /etc/apt/sources.list.d/pve-enterprise.list &&
-rm -rf /etc/apt/sources.list && cp -r sources.list /etc/apt/sources.list &&
+rm -rf /etc/apt/sources.list.d/pve-enterprise.list && rm -rf /etc/apt/sources.list &&
+touch /etc/apt/sources.list &&
+
+echo "deb http://deb.debian.org/debian/ stable-updates main contrib non-free" >> /etc/apt/sources.list &&
+echo "deb http://deb.debian.org/debian-security stable/updates main" >> /etc/apt/sources.list &&
+echo "deb http://ftp.debian.org/debian buster-backports main" >> /etc/apt/sources.list &&
+echo "deb http://deb.debian.org/debian/ stable main contrib non-free" >> /etc/apt/sources.list &&
+echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list &&
 
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg &&
+
+apt update && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common curl &&
 
 apt update && apt -y dist-upgrade && 
 
 wget http://software.virtualmin.com/gpl/scripts/install.sh && chmod a+x install.sh && ./install.sh -m -f -v &&
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+#curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
-echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" >> /etc/apt/sources.list &&
+#echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" >> /etc/apt/sources.list &&
 
-apt update && apt -y install docker-ce tasksel certbot gnome gdm3 xrdp &&
+apt update && apt -y install docker.io tasksel certbot gnome gdm3 xrdp &&
 
 docker volume create portainer_data &&
 
