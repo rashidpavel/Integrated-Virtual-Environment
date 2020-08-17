299dd7d943573923948628a1d7e7ee8b75d20634
rashidpavel <55319375+rashidpavel@users.noreply.github.com>
2020-08-17 14:00:42 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
-adduser sysadmin --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+adduser sysadmin --gecos "Sysadmin MRPlab,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "sysadmin:proxmox" |  chpasswd &&
 
-apt update && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common &&
+apt update && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common curl &&
 rm -rf /etc/apt/sources.list.d/pve-enterprise.list &&
 rm -rf /etc/apt/sources.list && cp -r sources.list /etc/apt/sources.list &&
 
@@ -11,7 +11,15 @@ apt update && apt -y dist-upgrade &&
 
 wget http://software.virtualmin.com/gpl/scripts/install.sh && chmod a+x install.sh && ./install.sh -m -f -v &&
 
-apt -y install tasksel docker.io tasksel certbot &&
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+
+echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" >> /etc/apt/sources.list &&
+
+apt update && apt -y install docker-ce tasksel certbot gnome gdm3 xrdp &&
+
+docker volume create portainer_data &&
+
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer &&
 
 reboot
 
