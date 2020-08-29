45b33aaef80f4ddcb6c06a59287671cdf7dfe859
rashidpavel <55319375+rashidpavel@users.noreply.github.com>
2020-08-29 12:27:59 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,34 +9,33 @@ echo "deb http://deb.debian.org/debian-security stable/updates main" >> /etc/apt
 echo "deb http://ftp.debian.org/debian buster-backports main" >> /etc/apt/sources.list &&
 echo "deb http://deb.debian.org/debian/ stable main contrib non-free" >> /etc/apt/sources.list &&
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list &&
-echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list &&
+#echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list &&
 
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg &&
-wget https://download.webmin.com/jcameron-key.asc &&
-apt-key add jcameron-key.asc &&
+#wget https://download.webmin.com/jcameron-key.asc &&
+#apt-key add jcameron-key.asc &&
 apt update && apt -y dist-upgrade && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common curl &&
 
 
-#wget http://software.virtualmin.com/gpl/scripts/install.sh && chmod a+x install.sh && ./install.sh -m -f -v &&
+wget http://software.virtualmin.com/gpl/scripts/install.sh && chmod a+x install.sh && ./install.sh -m -b LEMP -f -v &&
 
-#curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 
-#echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" >> /etc/apt/sources.list &&
+echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" >> /etc/apt/sources.list &&
 
 wget https://gandalfn.ovh/debian/pool/main/p/pantheon-debian-repos/pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
 
 dpkg -i pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
 
-apt update && apt -y install webmin cockpit tasksel certbot gnome pantheon pantheon-shell gdm3 xrdp &&
+apt update && apt -y install docker-ce cockpit cockpit-docker tasksel certbot pantheon pantheon-shell pantheon-extras lightdm xrdp &&
 
 apt -y remove network-manager &&
 
 apt -y install synaptic build-essential gparted &&
 
+docker volume create portainer_data &&
+
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer &&
+
 reboot
-#docker volume create portainer_data &&
-
-#docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer &&
-
-#reboot
 
