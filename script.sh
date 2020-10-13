b86a0553c963c4950aea04e4a7a813a0dc0c6d34
rashidpavel <55319375+rashidpavel@users.noreply.github.com>
2020-10-13 11:34:48 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
-adduser sysadmin --gecos "Sysadmin MRPlab,RoomNumber,WorkPhone,HomePhone" --disabled-password
-echo "sysadmin:x" |  chpasswd &&
+#adduser sysadmin --gecos "Sysadmin MRPlab,RoomNumber,WorkPhone,HomePhone" --disabled-password
+#echo "sysadmin:x" |  chpasswd &&
 
 rm -rf /etc/apt/sources.list.d/pve-enterprise.list && rm -rf /etc/apt/sources.list &&
 touch /etc/apt/sources.list &&
@@ -9,27 +9,31 @@ echo "deb http://deb.debian.org/debian-security stable/updates main" >> /etc/apt
 echo "deb http://ftp.debian.org/debian buster-backports main" >> /etc/apt/sources.list &&
 echo "deb http://deb.debian.org/debian/ stable main contrib non-free" >> /etc/apt/sources.list &&
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list &&
-#echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list &&
+echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list &&
 
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg &&
-#wget https://download.webmin.com/jcameron-key.asc &&
-#apt-key add jcameron-key.asc &&
+wget https://download.webmin.com/jcameron-key.asc &&
+apt-key add jcameron-key.asc &&
 apt update && apt -y dist-upgrade && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common curl &&
 
 
 wget http://software.virtualmin.com/gpl/scripts/install.sh && chmod a+x install.sh && ./install.sh -m -f -v &&
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - &&
 
-echo "deb [arch=amd64] https://download.docker.com/linux/debian buster stable" >> /etc/apt/sources.list &&
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable" &&
 
-wget https://gandalfn.ovh/debian/pool/main/p/pantheon-debian-repos/pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
 
-dpkg -i pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
+#wget https://gandalfn.ovh/debian/pool/main/p/pantheon-debian-repos/pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
 
-apt update && apt -y install docker-ce cockpit cockpit-docker tasksel certbot pantheon pantheon-shell pantheon-extras gnome gdm3 xrdp &&
+#dpkg -i pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
 
-apt -y remove network-manager &&
+apt update && apt -y install docker-ce docker-ce-cli containerd.io tasksel certbot gnome-flashback &&
+
+#apt -y remove network-manager &&
 
 apt -y install synaptic build-essential gparted &&
 
