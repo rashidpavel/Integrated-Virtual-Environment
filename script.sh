f3106c68fc28c7d311015ec16415779627d545b3
rashidpavel <55319375+rashidpavel@users.noreply.github.com>
2021-01-15 00:08:38 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,26 +17,26 @@ apt-key add jcameron-key.asc &&
 apt-get update && apt-get -y dist-upgrade && apt -y install lsb-release distro-info gnupg apt-transport-https software-properties-common curl ifupdown2 &&
 
 
-#wget http://software.virtualmin.com/gpl/scripts/install.sh && chmod a+x install.sh && ./install.sh -m -f -v &&
+wget http://software.virtualmin.com/gpl/scripts/install.sh && chmod a+x install.sh && ./install.sh -m -f -v &&
 
-#curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - &&
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - &&
 
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" &&
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" &&
 
 
 wget https://gandalfn.ovh/debian/pool/main/p/pantheon-debian-repos/pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
 
 dpkg -i pantheon-debian-repos_5.0-0+pantheon+buster+juno1_all.deb &&
 
-apt-get update && apt-get -y install webmin tasksel certbot apache2 php7.3 mariadb-server samba nfs-kernel-server task-desktop pantheon pantheon-shell gdm3 &&
+apt-get update && apt-get -y install tasksel certbot apache2 php7.3 mariadb-server samba nfs-kernel-server task-desktop pantheon pantheon-shell gdm3 docker-ce &&
 
 apt -y remove network-manager && network-manager-gnome &&
 
 apt -y install synaptic build-essential gparted &&
 
-#docker volume create portainer_data &&
+docker volume create portainer_data &&
 
-#docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer &&
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer &&
 
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
 
